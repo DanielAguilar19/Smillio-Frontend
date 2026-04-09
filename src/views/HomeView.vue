2960da267cd7c06f4383de50e7b2e913c9c1631b
Daniel Aguilar <aguilardaniel631@gmail.com>
2026-04-08 19:14:49 -0600

    commit inicial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+
+  <div class="p-4">
+    <Button label="Guardar" icon="pi pi-check" />
+  </div>
+</template>
